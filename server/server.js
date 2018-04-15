5c2084a90990407977875363ccc7c4234e712d93
Sandeep <muvva@usc.edu>
2018-04-15 15:38:01 -0700

    Set up seperate enivronment

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config');
 var express=require('express');
 var bodyParser=require('body-parser');
 const {ObjectID}=require('mongodb');
@@ -6,7 +7,7 @@ var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
 const _=require('lodash');
 var app=express();
-const port=process.env.PORT || 3000;
+const port=process.env.PORT ;
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res)=>{
